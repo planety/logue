1e2772e3b355c44015dd30a26dfa6b40fba4e2b9
PhilippMDoerner <philippmdoerner@web.de>
2022-07-12 19:33:48 +0200

    Fixed #134 "broken indentation"
    Issue #134 of the prologue project mentioned that the indentation here was non standard for nim projects.
    Since it's a really easy and quick fix I thought I might as well.

--- src/loguepkg/tml/app.nim
+++ src/loguepkg/tml/app.nim
@@ -5,11 +5,12 @@ import ./urls
 
 let
   env = loadPrologueEnv(".env")
-  settings = newSettings(appName = env.getOrDefault("appName", "Prologue"),
-                         debug = env.getOrDefault("debug", true),
-                         port = Port(env.getOrDefault("port", 8080)),
-                         secretKey = env.getOrDefault("secretKey", "")
-    )
+  settings = newSettings(
+    appName = env.getOrDefault("appName", "Prologue"),
+    debug = env.getOrDefault("debug", true),
+    port = Port(env.getOrDefault("port", 8080)),
+    secretKey = env.getOrDefault("secretKey", "")
+  )
 
 
 var app = newApp(settings = settings)
